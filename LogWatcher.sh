0dad377ffa3a0ce1facaf444f3cd4927afc8d2e4
bradykang <bradykang@tencent.com>
2016-07-17 20:45:51 +0800

    完成日志提取正则

--- LogWatcher.sh
+++ LogWatcher.sh
@@ -3,7 +3,7 @@
 # 偏移量文件格式：偏移量 指向文件
 
 # 预检查环境是否ok
-if [ ! -f $1 ]; then
+if [ ! -e $1 ]; then
 	echo "config file not exsit"
 	exit 1
 fi
@@ -16,6 +16,20 @@ if [ ! which inotifywait ]; then
 	exit 3
 fi
 
+dealLine(){
+	line=${1:1}
+	line=${line%?}
+	OLD_IFS="$IFS" 
+	IFS="][" 
+	arr=($a) 
+	IFS="$OLD_IFS" 
+	for s in ${arr[@]} 
+	do 
+	    echo "$s" 
+	done
+
+}
+
 # 处理日志文件函数
 # 参数列表  $1:起始偏移量；$2:结束偏移量；$3:文件名；$4:配置数组
 process(){
@@ -23,7 +37,11 @@ process(){
 	offsetEnd=$2
 	offsetFile=$3
 	configArr=$4
-	sed -n "${offsetStart},${offsetEnd}"p $offsetFile  | grep "dealName"
+
+
+	sed -n '1,2{/dealName/{s/a/k/p;}}'
+	sed "s/\[\(.*\)\]\[\(.*\)\]\[\(.*\)\]\[\(.*\)\]\[\(.*eventId[^0-9]*\([0-9]*\).*\)\]/insert into tLog (time,eventid) values(\'\1\',\'\2\',\'\3\',\'\4\',\'\5\',\'\6\')/g"
+	sed -n "${offsetStart},${offsetEnd}p" $offsetFile  | dealLine
 
 	sql="insert into table values('${filsds[0]}','${filsds[0]}')"
 
@@ -44,19 +62,21 @@ echo -e "parse config file succ \n\nstart watch log[log path:${configArr['logCat
 while watchInfo=`inotifywait -q --format '%e %f' -e modify,create ${configArr['logCategoryPath']}`;do
 	watchInfo=($watchInfo)
 	lines=`wc -l ${watchInfo[1]}`
+	lines=($lines)
+	lines=${lines[0]}
 	offsetInfo=`cat ${configArr['offsetFilePath']}`
 
 	# 如果偏移量文件不存在，则创建偏移量文件
 	if [ $? ]; then
 
 		# 如果偏移量记录文件为空，则初始化偏移量，从第0行读取变更的文件
-		if [ $offsetInfo -eq "" ]; then
+		if [ $offsetInfo == "" ]; then
 			process 1 ${lines} $watchInfo[1] $configArr
 
 		# 如果偏移量文件不为空，则取出偏移量和指向的文件
 		else
 			offsetInfo=($offsetInfo)
-			if [ ${offsetInfo[1]} -eq ${watchInfo[1]} ]; then
+			if [ ${offsetInfo[1]} != ${watchInfo[1]} ]; then
 				# 处理上一个日志文件
 				process ${offset} $ $offsetFile $configArr
 
@@ -68,7 +88,7 @@ while watchInfo=`inotifywait -q --format '%e %f' -e modify,create ${configArr['l
 			fi
 		fi
 		# 处理完成，更新偏移量和指向的文件
-		cat "$lines ${watchInfo[1]}" > ${configArr['offsetFilePath']}
+		echo "$lines ${watchInfo[1]}" > ${configArr['offsetFilePath']}
 	else
 		touch ${configArr['offsetFilePath']}
 	fi
